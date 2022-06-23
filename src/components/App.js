13ba9d2236385f6f252765993ec56f2df2d97d73
Benjamin Davis <ben.yj.davis@gmail.com>
2022-06-23 14:21:42 -0600

    Clarified some comments

--- src/components/App.js
+++ src/components/App.js
@@ -26,10 +26,10 @@ function App() {
       const search = await getDoc(doc(db,'PublicUserID',public_id))
       const User = await getDoc(
         doc(db,'Users',search.data()['private_reference']))
-      const userProjectsRef = await User.get('Projects')
+      const userProjectsRefs = await User.get('Projects')
 
-      // Promising each project from each user
-      const promise = userProjectsRef.map( async ref => {
+      // Promising each project from the user
+      const promise = userProjectsRefs.map( async ref => {
         const Doc = getDoc(ref['location'])
         return Doc
       });
