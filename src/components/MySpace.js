b40bdf7b2ee7627572cc0a81bb4b0dfdc963fa89
Bombshell05 <shannyve05@gmail.com>
2022-06-28 14:40:11 -0600

    I dont know what is going to happen so I'm sorry

--- src/components/MySpace.js
+++ src/components/MySpace.js
@@ -1,85 +1,104 @@
-import React from 'react';
+
 //import { db } from './firebase';
 //import {collection, addDoc } from 'firebase/firestore'
-import './MySpace.css' 
+import './MySpace.scss' 
+import React, { useState, useReducer } from 'react';
+import { v4 as uuid } from 'uuid';
+const initalNoteState ={
 
+    lastNoteCreated: null,
+    totalNotes: 0,
+    notes: [],
+};
+
+const notesReducer = (prevState,action) =>{
+    switch(action.type){
+        case 'ADD_NOTE':{
+            const newState = {
+                lastNoteCreated: new Date().toTimeString().slice(0,8),
+                totalNotes: prevState.notes.length + 1,
+                notes: [...prevState.notes, action.payload]
+            };
+
+            console.log('After ADD_NOTE: ', newState);
+            return newState;
+        }
+        case 'DELETE_NOTE':{
+            const newState = {
+                ...prevState,
+                notes: prevState.notes.filter(note => note.id !== action.payload.id),
+                totalNotes: prevState.notes.length - 1,
+            };
+            console.log('After DELETE_NOTE: ', newState);
+            return newState;
+        }
+            
+    }
+};
 
 function MySpace(){
-    const notesContainer = document.getElementById("app");
-    const addNoteButton = notesContainer.querySelector(".add-note");
+    const [noteInput, setNoteInput] = useState('');
+    const [noteState, dispatch] = useReducer(notesReducer, initalNoteState);
 
-    getNotes().forEach((note) => {
-    const noteElement = createNoteElement(note.id, note.content);
-    notesContainer.insertBefore(noteElement, addNoteButton);
-    });
-
-    addNoteButton.addEventListener("click", () => addNote());
-
-    function getNotes() {
-    return JSON.parse(localStorage.getItem("stickynotes-notes") || "[]");
-    }
-
-    function saveNotes(notes) {
-    localStorage.setItem("stickynotes-notes", JSON.stringify(notes));
-    }
-
-    function createNoteElement(id, content) {
-    const element = document.createElement("textarea");
-
-    element.classList.add("note");
-    element.value = content;
-    element.placeholder = "Empty Sticky Note";
-
-    element.addEventListener("change", () => {
-        updateNote(id, element.value);
-    });
-
-    element.addEventListener("dblclick", () => {
-        const doDelete = confirm(
-        "Are you sure you wish to delete this sticky note?"
-        );
-
-        if (doDelete) {
-        deleteNote(id, element);
+    const addNote = event => {
+        event.preventDefault();
+        if (!noteInput){
+            return;
         }
-    });
 
-    return element;
-    }
+        const newNote ={
+            id: uuid(),
+            text: noteInput,
+            rotate: Math.floor(Math.random() * 20),
+        };
 
-    function addNote() {
-        const notes = getNotes();
-        const noteObject = {
-        id: Math.floor(Math.random() * 100000),
-        content: ""
+        dispatch({type: 'ADD_NOTE', payload: newNote});
+        setNoteInput('');
+
     };
-
-    const noteElement = createNoteElement(noteObject.id, noteObject.content);
-    notesContainer.insertBefore(noteElement, addNoteButton);
-
-    notes.push(noteObject);
-    saveNotes(notes);
+    const dropNote = event => {
+        event.target.style.left = `${event.pageX - 50}px`;
+        event.target.style.top = `${event.pageY - 50}px`;
+    };
+    const dragOver = event => {
+        event.stopPropagation();
+        event.preventDefault();
     }
-
-    function updateNote(id, newContent) {
-        const notes = getNotes();
-        const targetNote = notes.filter((note) => note.id == id)[0];
-
-        targetNote.content = newContent;
-        saveNotes(notes);
-    }
-
-    function deleteNote(id, element) {
-        const notes = getNotes().filter((note) => note.id != id);
-
-    saveNotes(notes);
-    notesContainer.removeChild(element);
-    }
-    
 
     return(
-        <div id="app">
-        <button class="add-note" type="button">+</button>
+        <div className="app" onDragOver={dragOver}>
+            <h1>
+                My Notes ({ noteState.totalNotes})
+                <span>{noteState.notes.length ? `Last note created: ${noteState.lastNoteCreated}` : ' '}</span>
+            </h1>
+        <form onSubmit = {addNote} className = "note-form">
+            <textarea value = {noteInput}
+             onChange = {event => setNoteInput(event.target.value)}
+             placeholder = "Create a new note..."></textarea>
+            <button>Add</button>
+
+        </form>
+        {noteState
+          .notes
+          .map(note => (
+            <div className = "note"
+                style = {{transform: `rotate(${note.rotate}deg)`}}
+                draggable = "true"
+                onDragEnd={dropNote}
+                key = {note.id}>
+
+                <div onClick={() => dispatch({ type: 'DELETE_NOTE', payload: note })}
+                    className="close">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                        <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clipRule="evenodd" />
+                    </svg>
+                </div>
+                <pre className="text"> {note.text} </pre>
+            
+            </div>
+          ))
+        }
+
         </div>
     );
 }
